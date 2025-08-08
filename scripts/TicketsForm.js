98edb2497c5e837598f482d8541d0f93065916aa
artem-webdeveloper <ave_art@icloud.com>
2025-08-09 01:16:53 +0300

    feat: form

--- scripts/TicketsForm.js
+++ scripts/TicketsForm.js
@@ -4,11 +4,19 @@ class TicketsForm {
 
   _dateInfoEl = document.querySelector('#info-date');
   _timeInfoEl = document.querySelector('#info-time');
+
+  _formName = document.querySelector('#name');
   constructor() {
     this._dateEl.addEventListener('change', this._displayDate.bind(this));
     this._timeEl.addEventListener('change', this._displayTime.bind(this));
 
     document.addEventListener('DOMContentLoaded', this._correctDate.bind(this));
+
+    this._formName.addEventListener('input', this._validateName.bind(this));
+    this._formName.addEventListener('blur', () => {
+      const errorEl = document.querySelector('#error-name');
+      errorEl.style.display = 'none';
+    });
   }
 
   _displayDate() {
@@ -23,11 +31,41 @@ class TicketsForm {
   _correctDate() {
     const today = new Date().toISOString().split('T')[0];
     this._dateEl.min = today;
+    this._dateEl.value = today;
+
+    this._displayDate();
   }
 
   _displayTime() {
     const selectTime = document.querySelector('select[name="visit-time"]');
     this._timeInfoEl.textContent = selectTime.value.split(':').join(' : ');
+  }
+
+  _validateName() {
+    const errorEl = document.querySelector('#error-name');
+    console.log('input');
+
+    const value = this._formName.value.trim();
+
+    if (!/^[a-zA-Zа-яА-ЯёЁ\s]{3,15}$/.test(value)) {
+      this._formName.classList.remove('form__input--valid');
+      this._formName.classList.add('form__input--invalid');
+    } else {
+      this._formName.classList.remove('form__input--invalid');
+      this._formName.classList.add('form__input--valid');
+    }
+
+    if (value.length < 3) {
+      errorEl.textContent = 'Имя слишком короткое';
+      errorEl.style.display = 'block';
+    } else if (value.length > 15) {
+      errorEl.textContent = 'Имя слишком длинное';
+      errorEl.style.display = 'block';
+    } else if (!/^[a-zA-Zа-яА-ЯёЁ\s]+$/.test(value)) {
+      errorEl.textContent =
+        'Имя может содержать только русские или английские буквы';
+      errorEl.style.display = 'block';
+    } else errorEl.style.display = 'none';
   }
 }
 
